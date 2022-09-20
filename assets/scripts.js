846499adaf7efc98a902b25465fd46b57938c17d
Leanne <lkeeley@pace.edu>
2022-09-20 12:11:59 -0400

    add files

---
+++ assets/scripts.js
@@ -0,0 +1,18 @@
+let currentPrice=0, itemCount=0
+
+function addToCart(cookie) {
+    /* 
+        PRICES
+        peanut butter: 20
+        sandies: 30
+        party press: 35
+        chocolate chip: 25
+    */
+
+}
+
+function checkout() {
+    //Let your customer know how many items they are purchasing and the price
+    //Prompt the user for their name and address so you can mail them their purchase'
+    
+}
